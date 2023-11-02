c2710116ada876e7432afcbdffda667e7e6272ce
Kanatbek007 <147986453+Kanatbek007@users.noreply.github.com>
2023-11-02 11:57:37 +0600

    second commit

--- src/main.java
+++ src/main.java
@@ -1,9 +1,9 @@
 public class main {
     public static void main(String[] args) {
-        int x = 66;
+        int x = 666;
         int z = 11;
-        int y = 99;
-        System.out.println("Equels "+x*z*y + "som");
-        
+        int y = 1999;
+        System.out.println("Equels "+x + "som");
+
     }
 }
---
+++ src/namba.java
@@ -0,0 +1,9 @@
+public class namba {
+    public static void main(String[] args) {
+        String name = "Kanatbek";
+        int x = 10;
+        for(int i = 6 ; i < x ; i ++ ){
+            System.out.println(x);
+        }
+    }
+}
