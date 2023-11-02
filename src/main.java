a1c5ebfc403bc229116ce1b913bb0644cee47e46
Kanatbek007 <147986453+Kanatbek007@users.noreply.github.com>
2023-11-02 11:31:49 +0600

    firstCommit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        int x = 66;
+        int z = 11;
+        int y = 99;
+        System.out.println("Equels "+x*z*y + "som");
+        
+    }
+}
